351566bd0f08318f898d2d1807e2f2a8a4d6a969
Irene Yiu & Matthew Knudsen <ireyiu@gmail.com>
2014-03-07 15:45:56 -0800

    Fix issue with cube no longer shower after closure spike.   * Cube constructor variables were declared as var's. They should     have been set with this.<variable>   * Cube prototype was creating a THREE.Mesh with a cube's geometry     and material, but those were null because of the first issue.   * Move the Cube creation into a Factory method instead of something     that the Cube does.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function initialize() {
   world.setRender();
   world.setCameraPosition();
   world.setLighting();
-  var cube = new Cube( "blue" ).createCube()
+  var cube = new CubeFactory().createCube("blue");
   world.setScene( cube );
 
   world.render( cube );
@@ -17,55 +17,65 @@ World = function(){
   var camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, .1, 10 );
   var renderer = new THREE.WebGLRenderer();
 
-
   return {
     setRender: function() {
-      console.log("setRender")
       renderer.setSize( window.innerWidth, window.innerHeight );
       document.body.appendChild( renderer.domElement );
     },
 
     render: function( cube ) {
-      console.log("render")
       var self = this
       requestAnimationFrame( function(){
        self.render( cube )
       });
 
-      // cube.rotation.x += 0.1;
-      // console.log("its working")
+      cube.rotation.x += 0.1;
       renderer.render( scene, camera );
     },
 
     setCameraPosition: function(){
-      console.log("setCamera")
       camera.position.z = 3;
       camera.position.x = 3;
       camera.position.y = 1;
     },
 
     setLighting: function() {
-      console.log("setLighting")
       var directionalLight = new THREE.DirectionalLight(0xFFFFFF);
       directionalLight.position.set( 3,1,10 ).normalize();
       scene.add( directionalLight );
     },
 
     setScene: function( cube ) {
-      console.log("setScene")
       scene.add( cube );
     }
   }
 }
 
-function Cube( color ){
-  var geometry = new THREE.CubeGeometry(1,1,1);
-  var material = new THREE.MeshLambertMaterial( {color: 0x333} );
+function Cube( color ) {
+  this.geometry = new THREE.CubeGeometry(1,1,1);
+  this.material = new THREE.MeshLambertMaterial( {color: color} );
 }
 
-Cube.prototype = {
-  createCube: function(){
-    var cube = new THREE.Mesh( this.geometry, this.material );
-    return cube
-  }
+function CubeFactory() {
+  this.cubes = [];
 }
+
+CubeFactory.prototype = {
+  createCube: function(color) {
+    var cube = new Cube(color);
+    this.cubes.push(cube);
+    return new THREE.Mesh(cube.geometry, cube.material)
+  }
+};
+
+
+
+
+// Cube.prototype = {
+//   createCube: function() {
+//     console.log(this.geometry);
+//     console.log(this.material);
+//     var cube = new THREE.Mesh( this.geometry, this.material );
+//     return cube;
+//   }
+// }
