44e9f97ed658c89a6d0b467b4897659e3a741cac
Johnny Wu <jonathan.x.wu@gmail.com>
2014-03-07 01:00:44 -0800

    Refactors app into a World closure & Cube constructor function   * cube is not getting painted in the first place     * requestAnimationFrame problem?

--- app.js
+++ app.js
@@ -1,40 +1,71 @@
-window.addEventListener('load', initialize, false)
+window.addEventListener( 'load', initialize, false )
 
 function initialize() {
+  var world = new World()
+  world.setRender();
+  world.setCameraPosition();
+  world.setLighting();
+  var cube = new Cube( "blue" ).createCube()
+  world.setScene( cube );
+
+  world.render( cube );
+}
+
+
+World = function(){
   var scene = new THREE.Scene();
-      //camera(zoom,someshit,not sure, near, far) READ DOCS MAYBE GUISE
-      //also whats a frustum
-      var camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, .1, 10);
-      var renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(renderer.domElement);
+  var camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, .1, 10 );
+  var renderer = new THREE.WebGLRenderer();
 
-      //THE WORST IDEAAAAA
-      var geometry = new THREE.CubeGeometry(1,1,1);
-      //we can also set textures
-      var material = new THREE.MeshLambertMaterial({color: 0x333333});
-      //set shape of geo
-      var cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
 
+  return {
+    setRender: function() {
+      console.log("setRender")
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.body.appendChild( renderer.domElement );
+    },
+
+    render: function( cube ) {
+      console.log("render")
+      var self = this
+      requestAnimationFrame( function(){
+       self.render( cube )
+      });
+
+      // cube.rotation.x += 0.1;
+      // console.log("its working")
+      renderer.render( scene, camera );
+    },
+
+    setCameraPosition: function(){
+      console.log("setCamera")
       camera.position.z = 3;
       camera.position.x = 3;
       camera.position.y = 1;
-      console.log(camera)
+    },
 
-      // var ambientLight = new THREE.AmbientLight(0x00044);
-      // scene.add(ambientLight)
-
+    setLighting: function() {
+      console.log("setLighting")
       var directionalLight = new THREE.DirectionalLight(0xFFFFFF);
-      directionalLight.position.set(3,1,10).normalize();
-      scene.add(directionalLight);
+      directionalLight.position.set( 3,1,10 ).normalize();
+      scene.add( directionalLight );
+    },
 
-      var render = function () {
-        requestAnimationFrame(render);
-        cube.rotation.x += 0.1;
-        renderer.render(scene, camera);
-      };
-
-      render();
-
+    setScene: function( cube ) {
+      console.log("setScene")
+      scene.add( cube );
     }
+  }
+}
+
+function Cube( color ){
+  var geometry = new THREE.CubeGeometry(1,1,1);
+  var material = new THREE.MeshLambertMaterial( {color: 0x333} );
+}
+
+Cube.prototype = {
+  createCube: function(){
+    var cube = new THREE.Mesh( this.geometry, this.material );
+    return cube
+  }
+}
