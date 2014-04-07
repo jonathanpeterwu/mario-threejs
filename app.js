df4c5456d8edca26454ad4cff2be2357826bf7f0
Salar Khan <salar.k3@gmail.com>
2014-04-07 02:13:36 -0700

    replaces Factory design with a simple array of Cube objects   * pulls THREE geometry and material out of the Cube constructor function     * we don't need to create a new geometry for every single cube. we can       just past that value in when we create each mesh   * attempting to use THREE.Color to manipulate each mesh instead of creating     a new material every time, too   * removes unnecessary semicolons   * moves Cube related functions up to the top, since that's what we're looking     at most of the time, anyway

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
-window.addEventListener('load', initialize);
+window.addEventListener('load', initialize)
 
 function initialize() {
+  // world init
   var world = new World()
-  world.setRender();
-  world.setCameraPosition();
-  world.setLighting();
+  world.setRender()
+  world.setCameraPosition()
+  world.setLighting()
 
-  var marioFactory = new CubeFactory()
+  // geometry,mesh,material init
+  var geometry = new THREE.CubeGeometry( 1, 1, 1 )
+  var material = new THREE.MeshLambertMaterial()
   var mergedMarioGeometry = new THREE.Geometry()
+  var cubes = []
 
   // creates Cube objects from our mario JSON
   for ( var i = 0; i < mario.cubeAttributes.length; i++ ){
-    marioFactory.createCube( mario.cubeAttributes[ i ].color, mario.cubeAttributes[ i ].position, mario.cubeAttributes[ i ].color)
-}
-
+    cubes.push ( new Cube( geometry, mario.cubeAttributes[ i ].position, material, mario.cubeAttributes[ i ].color ) )
+  }
+  console.log ( cubes )
   // merge each Cube's mesh into the mergedMarioGeometry.
   // we need to figure out a way to also apply the correct material (read: color) at this step
-  for ( var i = 0; i < marioFactory.cubes.length; i++ ){
-    THREE.GeometryUtils.merge( mergedMarioGeometry, marioFactory.cubes[ i ].mesh)
-  };
+  for ( var i = 0; i < cubes.length; i++ ){
+    THREE.GeometryUtils.merge( mergedMarioGeometry, cubes[ i ].mesh )
+  }
 
-  // merge the merged geometry into a mesh so that we can insert it into the
-  // scene
-  var mergedMarioMesh = new THREE.Mesh( mergedMarioGeometry)
-  //, new
-  //THREE.MeshBasicMaterial({ color: 0x00ff00 }) );
-
-  console.log(mergedMarioMesh)
-  world.setScene( mergedMarioMesh );
-  world.render( mergedMarioMesh );
+  console.log ( mergedMarioGeometry )
+  var mergedMarioMesh = new THREE.Mesh( mergedMarioGeometry ,new THREE.MeshLambertMaterial( {color: "#000"}) )
+  world.setScene( mergedMarioMesh )
+  world.render( mergedMarioMesh )
 }
+
+function Cube( geometry, position, material, color ) {
+  this.color = new THREE.Color( color )
+  this.mesh = new THREE.Mesh( geometry, material )
+  this.mesh.position.x = position.x
+  this.mesh.position.y = position.y
+  this.mesh.position.z = position.z
+}
+
 World = function(){
   this.scene = new THREE.Scene();
   this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, .1, 100 );
@@ -68,26 +76,5 @@ World.prototype = {
 
   setScene: function( mesh ) {
     this.scene.add( mesh );
-  }
-}
-
-function Cube( color, position ) {
-  this.geometry = new THREE.CubeGeometry( 1, 1, 1 );
-  this.material = new THREE.MeshLambertMaterial( {color: color} );
-  this.mesh = new THREE.Mesh( this.geometry, this.material )
-  this.mesh.position.x = position.x
-  this.mesh.position.y = position.y
-  this.mesh.position.z = position.z
-}
-
-function CubeFactory() {
-  this.cubes = [];
-}
-
-CubeFactory.prototype = {
-  createCube: function( color, position ){
-    var cube = new Cube( color, position );
-    this.cubes.push( cube );
-    return cube
   }
 }
